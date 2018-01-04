d600b633658e1a29e2239bd1ffe8aede5b0f0df3
lewismck <lewisamckeown@gmail.com>
2018-01-04 20:32:44 +0000

    Changed input to text area and removed some redundant includes

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const program = 'mul 3 sub 2 sum 1 3 4';
 
-//parse str with lexer -> parse it -> transpile it -> return output as jS
+//parse str with lexer -> parse it -> transpile it -> return JS output as string
 const test = str => transpile(parse(lex(str)));
 
 const test2 = () => transpile(parse(lex(program)));
